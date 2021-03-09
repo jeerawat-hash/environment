d890dee8498bcc6a614ff9ca5b5448cc9ebb576a
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 22:32:21 +0700

    1

--- application/views/page/ManageStore/2.php
+++ application/views/page/ManageStore/2.php
@@ -1,3 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.js"></script>
+
 <div class="main-panel">
 			<div class="content">
 				<div class="page-inner">
