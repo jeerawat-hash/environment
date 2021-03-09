2aca00e2213fb1b047091259f8e42edf3d8ebc42
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 20:43:34 +0700

    add

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,30 +1,27 @@
 <?php 
 
-		$Dashboard = array("", array("","","","","","","","","",""),"" );
-		$ManageStore = array("", array("","","","","","","","","",""),"" ); 
-		$ManageSystem = array("", array("","","","","","","","","",""),"" ); 
+		$Dashboard = array("", array("","","","","","","","","","") );
+		$ManageStore = array("", array("","","","","","","","","","") ); 
+		$ManageSystem = array("", array("","","","","","","","","","") ); 
 
 		switch ($page_focus) {
 			case 'Dashboard':
 				
-				$Dashboard[0] = "active submenu";
-				$Dashboard[2] = "show";
+				$Dashboard[0] = "active";
 				$Dashboard[1][$page_menu] = "active";
 
 				break;
 			
 			case 'ManageStore':
 				
-				$ManageStore[0] = "active submenu";
-				$ManageStore[2] = "show";
+				$ManageStore[0] = "active";
 				$ManageStore[1][$page_menu] = "active";
 
 				break;
 
 			case 'ManageSystem':
 				
-				$ManageSystem[0] = "active submenu";
-				$ManageSystem[2] = "show";
+				$ManageSystem[0] = "active";
 				$ManageSystem[1][$page_menu] = "active";
 
 				break;
@@ -142,7 +139,7 @@
 								<p>ผลการดำเนินงาน</p>
 								<span class="caret"></span>
 							</a>
-							<div class="collapse <?php echo $Dashboard[2]; ?>" id="Dashboard">
+							<div class="collapse" id="Dashboard">
 								<ul class="nav nav-collapse">
 									<li class="<?php echo $Dashboard[1][0]; ?>">
 										<a href="https://environment.webclient.me/index.php/Dashboard">
@@ -175,7 +172,7 @@
 						</li>
 
                         <li class="nav-item <?php echo $ManageStore[0]; ?>">
-							<a data-toggle="collapse <?php echo $ManageStore[2]; ?>" href="#ManageStore" class="collapsed" aria-expanded="false">
+							<a data-toggle="collapse" href="#ManageStore" class="collapsed" aria-expanded="false">
 								<i class="fas fa-hockey-puck"></i>
 								<p>บริหารการจัดเก็บ</p>
 								<span class="caret"></span>
@@ -202,7 +199,7 @@
 						</li>
 
                         <li class="nav-item <?php echo $ManageSystem[0]; ?>">
-							<a data-toggle="collapse <?php echo $ManageSystem[2]; ?>" href="#ManageSystem" class="collapsed" aria-expanded="false">
+							<a data-toggle="collapse" href="#ManageSystem" class="collapsed" aria-expanded="false">
 								<i class="fas fa-database"></i>
 								<p>จัดการข้อมูล</p>
 								<span class="caret"></span>
