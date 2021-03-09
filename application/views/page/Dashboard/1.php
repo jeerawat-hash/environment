da1d6dc84ce4af4f756fc97d86afb78c1523d619
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 21:52:09 +0700

    add

--- application/views/page/Dashboard/1.php
+++ application/views/page/Dashboard/1.php
@@ -5,6 +5,48 @@
                 <div class="page-header">
 						<h4 class="page-title"><?php echo $page_name; ?></h4> 
 					</div>
+
+
+
+					<div class="row">
+						<div class="col-md-4">
+							<div class="card card-dark bg-primary-gradient">
+								<div class="card-body pb-0">
+									<div class="h1 fw-bold float-right">+5%</div>
+									<h2 class="mb-2">17</h2>
+									<p>Users online</p>
+									<div class="pull-in sparkline-fix chart-as-background">
+										<div id="lineChart"><canvas width="327" height="70" style="display: inline-block; width: 327px; height: 70px; vertical-align: top;"></canvas></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card card-dark bg-secondary-gradient">
+								<div class="card-body pb-0">
+									<div class="h1 fw-bold float-right">-3%</div>
+									<h2 class="mb-2">27</h2>
+									<p>New Users</p>
+									<div class="pull-in sparkline-fix chart-as-background">
+										<div id="lineChart2"><canvas width="327" height="70" style="display: inline-block; width: 327px; height: 70px; vertical-align: top;"></canvas></div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card card-dark bg-success2">
+								<div class="card-body pb-0">
+									<div class="h1 fw-bold float-right">+7%</div>
+									<h2 class="mb-2">213</h2>
+									<p>Transactions</p>
+									<div class="pull-in sparkline-fix chart-as-background">
+										<div id="lineChart3"><canvas width="327" height="70" style="display: inline-block; width: 327px; height: 70px; vertical-align: top;"></canvas></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
 					<div class="row">
 						<div class="col-md-6">
 							<div class="card full-height">
