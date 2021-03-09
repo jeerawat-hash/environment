f448adf968c8d7f1434251e4ecdebd63e5c07060
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 22:48:53 +0700

    1

--- application/views/page/ManageStore/2.php
+++ application/views/page/ManageStore/2.php
@@ -72,8 +72,10 @@
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          
           <h4 class="modal-title">Modal Header</h4>
+
+		  <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
           <p>Some text in the modal.</p>
