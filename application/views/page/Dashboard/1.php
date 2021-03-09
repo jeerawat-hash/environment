63560437138f214d619a9b97c13de38eadb53c6a
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 22:00:58 +0700

    add

--- application/views/page/Dashboard/1.php
+++ application/views/page/Dashboard/1.php
@@ -103,27 +103,9 @@
 			</div>
 			<footer class="footer">
 				<div class="container-fluid">
-					<nav class="pull-left">
-						<ul class="nav">
-							<li class="nav-item">
-								<a class="nav-link" href="https://www.themekita.com">
-									ThemeKita
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
-									Help
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
-									Licenses
-								</a>
-							</li>
-						</ul>
-					</nav>
+					 
 					<div class="copyright ml-auto">
-						2018, made with <i class="fa fa-heart heart text-danger"></i> by <a href="https://www.themekita.com">ThemeKita</a>
+						2021, Create by <i class="fa fa-heart heart text-danger"></i> by <a href="#">p.jeerawat</a>
 					</div>				
 				</div>
 			</footer>
