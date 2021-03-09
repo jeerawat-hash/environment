db082633f5352b7ff6120fc642d0bcebdef159d6
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 18:14:13 +0700

    add

--- application/views/home.php
+++ application/views/home.php
@@ -31,8 +31,7 @@
 			<!-- Logo Header -->
 			<div class="logo-header" data-background-color="dark">
 				
-				<a href="index.html" class="logo">
-					<!--<img src="../assets/img/logo.svg" alt="navbar brand" class="navbar-brand">-->
+				<a href="#" class="logo"> 
                     <h2 style="color: white;" class="navbar-brand">EnvironmentSystem</h2>
 				</a>
 				<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
