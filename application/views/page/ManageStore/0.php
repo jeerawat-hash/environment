1ebcdcc972697f49b83c9b99d894de0f7ecf6642
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-09 21:26:21 +0700

    add

--- application/views/page/ManageStore/0.php
+++ application/views/page/ManageStore/0.php
@@ -9,26 +9,119 @@
 
 
 
-                        <div class="col-md-12">
+                    <div class="col-md-12">
 							<div class="card">
 								<div class="card-header">
-									<h4 class="card-title">แผนที่</h4>
+									<div class="card-head-row card-tools-still-right">
+										<h4 class="card-title">แผนที่การปฏิบัติงาน</h4>
+										<div class="card-tools">
+											<button class="btn btn-icon btn-link btn-primary btn-xs"><span class="fa fa-angle-down"></span></button>
+											<button class="btn btn-icon btn-link btn-primary btn-xs btn-refresh-card"><span class="fa fa-sync-alt"></span></button>
+											<button class="btn btn-icon btn-link btn-primary btn-xs"><span class="fa fa-times"></span></button>
+										</div>
+									</div>
+									<p class="card-category">
+									แผนที่แสดงข้อมูลตำแหน่งถังขยะที่ถูกจัดเก็บแล้ว</p>
 								</div>
 								<div class="card-body">
-									<div class="table-responsive">
-										 
-
-
-
-
-
-
-
-
-
-
-
-
+									<div class="row">
+										<div class="col-md-6">
+											<div class="table-responsive table-hover table-sales">
+												<table class="table">
+													<tbody>
+														<tr>
+															<td>
+																<div class="flag">
+																	<img src="https://environment.webclient.me/assets/img/flags/id.png" alt="indonesia">
+																</div>
+															</td>
+															<td>Indonesia</td>
+															<td class="text-right">
+																2.320
+															</td>
+															<td class="text-right">
+																42.18%
+															</td>
+														</tr>
+														<tr>
+															<td>
+																<div class="flag">
+																	<img src="https://environment.webclient.me/assets/img/flags/us.png" alt="united states">
+																</div>
+															</td>
+															<td>USA</td>
+															<td class="text-right">
+																240
+															</td>
+															<td class="text-right">
+																4.36%
+															</td>
+														</tr>
+														<tr>
+															<td>
+																<div class="flag">
+																	<img src="https://environment.webclient.me/assets/img/flags/au.png" alt="australia">
+																</div>
+															</td>
+															<td>Australia</td>
+															<td class="text-right">
+																119
+															</td>
+															<td class="text-right">
+																2.16%
+															</td>
+														</tr>
+														<tr>
+															<td>
+																<div class="flag">
+																	<img src="https://environment.webclient.me/assets/img/flags/ru.png" alt="russia">
+																</div>
+															</td>
+															<td>Russia</td>
+															<td class="text-right">
+																1.081
+															</td>
+															<td class="text-right">
+																19.65%
+															</td>
+														</tr>
+														<tr>
+															<td>
+																<div class="flag">
+																	<img src="https://environment.webclient.me/assets/img/flags/cn.png" alt="china">
+																</div>
+															</td>
+															<td>China</td>
+															<td class="text-right">
+																1.100
+															</td>
+															<td class="text-right">
+																20%
+															</td>
+														</tr>
+														<tr>
+															<td>
+																<div class="flag">
+																	<img src="https://environment.webclient.me/assets/img/flags/br.png" alt="brazil">
+																</div>
+															</td>
+															<td>Brasil</td>
+															<td class="text-right">
+																640
+															</td>
+															<td class="text-right">
+																11.63%
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="mapcontainer">
+												<div id="map-example" class="vmap"></div>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
