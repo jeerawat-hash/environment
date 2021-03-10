54f26d8852158303facfa130110c15e9509c39cd
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-10 23:08:46 +0700

    11

--- application/views/page/ManageStore/2.php
+++ application/views/page/ManageStore/2.php
@@ -16,7 +16,9 @@
 						<div class="col-md-12">
 							<div class="card">
 								<div class="card-header">
-									<h4 class="">กลุ่มผู้ปฏิบัติงานขณะนี้  <p align="right" >  <button class="btn btn-warning"  id="btncreate" > สร้างกลุ่ม</button>  </p> </h4>  
+									<h4 class="">กลุ่มผู้ปฏิบัติงานขณะนี้  
+									 <p align="right" >  <button class="btn btn-warning"  id="btncreate" > สร้างกลุ่ม</button>
+									<p align="right" >  <button class="btn btn-warning"  id="btncreate1" > จัดกลุ่ม</button>  </p> </h4>  
 									<p class="card-category">
 									  
 									       
@@ -115,7 +117,7 @@
 															<div class="col-md-6 col-lg-6">
 																<div class="form-group">
 																	<label for="email2">รายละเอียด</label>
-																	<input type="email" class="form-control" id="data3" placeholder="กรุณากรอกเบอร์โทร">
+																	<input type="email" class="form-control" id="data3" placeholder="กรุณากรอกรายละเอียด">
 																	
 																</div>
 															</div>
@@ -138,7 +140,78 @@
 					</div>
 					 
 					<!-- END  Modal -->
-          		 
+ 
+													
+					<!-- Modal -->
+					<div class="modal fade" id="myModal1" role="dialog">
+						<div class="modal-dialog modal-lg">
+						
+						<!-- Modal content-->
+						<div class="modal-content">
+							<div class="modal-header  bg-success ">
+							
+							<h4 style="color:white"  >ลงทะเบียนผู้ปฏิบัติงาน</h4>
+
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							</div>
+							<div class="modal-body">
+
+							<div class="row">
+											<div class="col-md-12">
+												<div class="card">
+													<div class="card-header">
+														<div class="card-title">สร้างลงทะเบียนผู้ปฏิบัติงาน</div>
+													</div>
+													<div class="card-body">
+														<div class="row">
+														<div class="col-md-6 col-lg-6">
+															<div class="form-group">
+																<label for="smallSelect">ป้ายทะเบียน</label>
+															
+																<select class="form-control form-control-sm" id="smallSelect">
+																	<option>1</option>
+																	<option>2</option>
+																	<option>3</option>
+																	<option>4</option>
+																	<option>5</option>
+																</select>
+														
+																
+															</div>
+														</div>
+															<div class="col-md-6 col-lg-6">
+																<div class="form-group">
+																	<label for="data2">ผู้ดูเเล</label>
+																	<select class="form-control form-control-sm" id="data2">
+																	<option>1</option>
+																	<option>2</option>
+																	<option>3</option>
+																	<option>4</option>
+																	<option>5</option>
+																</select>
+																
+																</div>
+															</div>
+															
+														</div>
+														 
+													</div>
+
+												</div>
+											</div>
+							</div>
+							
+							</div>
+							<div class="modal-footer">
+							<button type="button" id="btncreatedata1" class="btn btn-warning"  >สร้าง</button>
+							<button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+							</div>
+						</div>
+						
+						</div>
+					</div>
+					 
+					<!-- END  Modal -->
 					</div>
 				</div>
 			</div>
@@ -172,6 +245,10 @@
 				$('#myModal').modal('show'); 
               });
  
+			  $("#btncreate1").click(function(){
+				$('#myModal1').modal('show'); 
+              });
+
 			  $("#btncreatedata").click(function(){
 				  var smallSelect = $("#smallSelect").val();
 				  var data2 = $("#data2").val(); 
@@ -225,6 +302,51 @@
 					});
               });
 
+			  $("#btncreatedata1").click(function(){
+				  var smallSelect = $("#smallSelect").val();
+				  var data2 = $("#data2").val(); 
+				  var data3 = $("#data3").val(); 
+				  if( smallSelect == ""){
+					swal("กรุณากรอกป้ายทะเบียน", {
+						buttons: {        			
+							confirm: {
+								className : 'btn btn-danger'
+							}
+						},
+					});		 
+					  $( "#data1" ).focus();
+					  return false;
+				  }
+				  if( data2 == ""){
+					swal("กรุณากรอกผู้ดูแล", {
+						buttons: {        			
+							confirm: {
+								className : 'btn btn-danger'
+							}
+						},
+					});		 
+					  $( "#data2" ).focus();
+					  return false;
+				  } 
+				  				  				  
+				swal({
+						title: "บันทึกเรียบร้อย",
+						text: " ",
+						icon: "success",
+						buttons: {
+							confirm: {
+								text: "ตกลง",
+								value: true,
+								visible: true,
+								className: "btn btn-success",
+								closeModal: true
+							}
+						}
+					});
+              });
+
+
+			   
 			   
 			  $("#btndelete").click(function(){
 				  				  				  
