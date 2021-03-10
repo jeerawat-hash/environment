23c550da8f737a333a77a4fa467de9b126462f0b
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-10 19:39:06 +0700

    2

--- application/views/page/ManageStore/2.php
+++ application/views/page/ManageStore/2.php
@@ -78,6 +78,36 @@
 		  <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
+
+		<div class="row">
+						<div class="col-md-12">
+							<div class="card">
+								<div class="card-header">
+									<div class="card-title">Form Elements</div>
+								</div>
+								<div class="card-body">
+									<div class="row">
+										<div class="col-md-6 col-lg-4">
+											<div class="form-group">
+												<label for="email2">Email Address</label>
+												<input type="email" class="form-control" id="email2" placeholder="Enter Email">
+												<small id="emailHelp2" class="form-text text-muted">We'll never share your email with anyone else.</small>
+											</div>
+										</div>
+
+										<div class="col-md-6 col-lg-4">
+											<div class="form-group">
+												<label for="email2">Email Address</label>
+												<input type="email" class="form-control" id="email2" placeholder="Enter Email">
+												<small id="emailHelp2" class="form-text text-muted">We'll never share your email with anyone else.</small>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+		</div>
           <p>Some text in the modal.</p>
         </div>
         <div class="modal-footer">
