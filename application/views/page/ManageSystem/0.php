645d2943ede2d5f2fddc782604562b40bee23560
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-10 21:47:58 +0700

    1

--- application/views/page/ManageSystem/0.php
+++ application/views/page/ManageSystem/0.php
@@ -13,7 +13,7 @@
 									<h4 class="card-title">ผู้ปฏิบัติงาน</h4>
 									<p class="card-category">
 									แสดงผลข้อมูลผู้ปฏิบัติงาน 
-									<p align="right" >  <button class="btn btn-warning"  id="btncreate" > สร้างรายการการผู้ปฏิบัติงาน</button>  </p>
+									<p align="right" >  <button class="btn btn-warning"  id="btncreate" > เพิ่มรายการการผู้ปฏิบัติงาน</button>  </p>
 								</p>
 								</div>
 								
@@ -54,7 +54,7 @@
 									<h4 class="card-title">ผู้ใช้งานระบบ</h4>
 									<p class="card-category">
 									แสดงผลข้อมูลบุคคลากร
-									<p align="right" >  <button class="btn btn-warning"  id="btncreatesystem" > สร้างรายการผู้ใช้งานระบบ</button>  </p>
+									<p align="right" >  <button class="btn btn-warning"  id="btncreatesystem" > เพิ่มรายการผู้ใช้งานระบบ</button>  </p>
 								</p>
 								</div>
 								
@@ -98,15 +98,15 @@
 			</div>
 
 	
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
+  <!-- Modal 1-->
+  <div class="modal fade" id="Modalone" role="dialog">
     <div class="modal-dialog modal-lg">
     
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header  bg-success ">
           
-          <h4 style="color:white"  >รายละเอียดการจัดเก็บ</h4>
+          <h4 style="color:white"  >สร้างรายการผู้ปฏิบัติงาน</h4>
 
 		  <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
@@ -116,48 +116,24 @@
 						<div class="col-md-12">
 							<div class="card">
 								<div class="card-header">
-									<div class="card-title">สร้างรายละเอียดการจัดเก็บ</div>
+									<div class="card-title">สร้างรายการผู้ปฏิบัติงาน</div>
 								</div>
 								<div class="card-body">
-									<div class="row">
+								<div class="row">
 										<div class="col-md-6 col-lg-6">
 											<div class="form-group">
-												<label for="email2">รหัสพนักงาน</label>
+												<label for="email2">ชื่อ</label>
 												<input type="email" class="form-control" id="email2" placeholder="รหัสพนักงาน"> 
 											</div>
 										</div>
 										<div class="col-md-6 col-lg-6">
 											<div class="form-group">
-												<label for="email2">ชื่อพนักงาน</label>
+												<label for="email2">เบอร์โทร</label>
 												<input type="email" class="form-control" id="email2" placeholder="ชื่อพนักงาน">
 											 
 											</div>
 										</div>
 										 
-									</div>
-									<div class="row">
-										<div class="col-md-6 col-lg-6">
-											<div class="form-group">
-												<label for="email2">แผนก</label>
-												<input type="email" class="form-control" id="email2" placeholder="แผนก">
-												 
-											</div>
-										</div>
-										<div class="col-md-6 col-lg-6">
-											<div class="form-group">
-												<label for="smallSelect">ประเภทการจัดเก็บ</label>
-											  
-												<select class="form-control form-control-sm" id="smallSelect">
-													<option>1</option>
-													<option>2</option>
-													<option>3</option>
-													<option>4</option>
-													<option>5</option>
-												</select>
-										 
-												 
-											</div>
-										</div>
 									</div>
 								</div>
 
@@ -167,7 +143,67 @@
           
         </div>
         <div class="modal-footer">
-          <button type="button" id="btncreatedata" class="btn btn-warning"  >สร้าง</button>
+          <button type="button" id="btncreatedataone" class="btn btn-warning"  >สร้าง</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+  <!-- END  Modal 1 -->
+
+
+
+  
+  <!-- Modal 2-->
+  <div class="modal fade" id="Modaltwo" role="dialog">
+    <div class="modal-dialog modal-lg">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header  bg-success ">
+          
+          <h4 style="color:white"  >สร้างผู้ใช้งานระบบ</h4>
+
+		  <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+
+		<div class="row">
+						<div class="col-md-12">
+							<div class="card">
+								<div class="card-header">
+									<div class="card-title">สร้างผู้ใช้งานระบบ</div>
+								</div>
+								<div class="card-body">
+									<div class="row">
+										<div class="col-md-6 col-lg-6">
+											<div class="form-group">
+												<label for="email2">ชื่อ</label>
+												<input type="email" class="form-control" id="email2" placeholder="รหัสพนักงาน"> 
+											</div>
+										</div>
+										<div class="col-md-6 col-lg-6">
+											<div class="form-group">
+												<label for="email2">เบอร์โทร</label>
+												<input type="email" class="form-control" id="email2" placeholder="ชื่อพนักงาน">
+											 
+											</div>
+										</div>
+										 
+									</div>
+									 
+								</div>
+
+							</div>
+						</div>
+		</div>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="button" id="btncreatedatatwo" class="btn btn-warning"  >สร้าง</button>
           <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
         </div>
       </div>
@@ -205,14 +241,14 @@
 
             });
 			$("#btncreate").click(function(){
-				$('#myModal').modal('show'); 
+				$('#Modalone').modal('show'); 
               });
 
 			  $("#btncreatesystem").click(function(){
-				$('#myModal').modal('show'); 
+				$('#Modaltwo').modal('show'); 
               });
  
-			  $("#btncreatedata").click(function(){
+			  $("#btncreatedataone").click(function(){
 				swal({
 						title: "บันทึกเรียบร้อย",
 						text: " ",
@@ -229,7 +265,22 @@
 					});
               });
 			   
-			   
+			  $("#btncreatedatatwo").click(function(){
+				swal({
+						title: "บันทึกเรียบร้อย",
+						text: " ",
+						icon: "success",
+						buttons: {
+							confirm: {
+								text: "ตกลง",
+								value: true,
+								visible: true,
+								className: "btn btn-success",
+								closeModal: true
+							}
+						}
+					});
+              });		   
         </script>
 
  
