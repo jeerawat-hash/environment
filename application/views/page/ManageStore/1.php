b3f24fe2ac436c2c33767e18b8f62d56b558419c
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-10 21:57:28 +0700

    111

--- application/views/page/ManageStore/1.php
+++ application/views/page/ManageStore/1.php
@@ -53,90 +53,89 @@
 
 						 
 					</div>
+				 
+
+					
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" role="dialog">
+					<div class="modal-dialog modal-lg">
+					
+					<!-- Modal content-->
+					<div class="modal-content">
+						<div class="modal-header  bg-success ">
+						
+						<h4 style="color:white"  >รายละเอียดการจัดเก็บ</h4>
+
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						</div>
+						<div class="modal-body">
+
+						<div class="row">
+										<div class="col-md-12">
+											<div class="card">
+												<div class="card-header">
+													<div class="card-title">สร้างรายละเอียดการจัดเก็บ</div>
+												</div>
+												<div class="card-body">
+													<div class="row">
+														<div class="col-md-6 col-lg-6">
+															<div class="form-group">
+																<label for="email2">รหัสพนักงาน</label>
+																<input type="email" class="form-control" id="email2" placeholder="รหัสพนักงาน"> 
+															</div>
+														</div>
+														<div class="col-md-6 col-lg-6">
+															<div class="form-group">
+																<label for="email2">ชื่อพนักงาน</label>
+																<input type="email" class="form-control" id="email2" placeholder="ชื่อพนักงาน">
+															
+															</div>
+														</div>
+														
+													</div>
+													<div class="row">
+														<div class="col-md-6 col-lg-6">
+															<div class="form-group">
+																<label for="email2">แผนก</label>
+																<input type="email" class="form-control" id="email2" placeholder="แผนก">
+																
+															</div>
+														</div>
+														<div class="col-md-6 col-lg-6">
+															<div class="form-group">
+																<label for="smallSelect">ประเภทการจัดเก็บ</label>
+															
+																<select class="form-control form-control-sm" id="smallSelect">
+																	<option>1</option>
+																	<option>2</option>
+																	<option>3</option>
+																	<option>4</option>
+																	<option>5</option>
+																</select>
+														
+																
+															</div>
+														</div>
+													</div>
+												</div>
+
+											</div>
+										</div>
+						</div>
+						
+						</div>
+						<div class="modal-footer">
+						<button type="button" id="btncreatedata" class="btn btn-warning"  >สร้าง</button>
+						<button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+						</div>
+					</div>
+					
+					</div>
+				</div>
+				 
+				<!-- END  Modal -->
 				</div>
 			</div>
-
-	
-  <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog modal-lg">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header  bg-success ">
-          
-          <h4 style="color:white"  >รายละเอียดการจัดเก็บ</h4>
-
-		  <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        <div class="modal-body">
-
-		<div class="row">
-						<div class="col-md-12">
-							<div class="card">
-								<div class="card-header">
-									<div class="card-title">สร้างรายละเอียดการจัดเก็บ</div>
-								</div>
-								<div class="card-body">
-									<div class="row">
-										<div class="col-md-6 col-lg-6">
-											<div class="form-group">
-												<label for="email2">รหัสพนักงาน</label>
-												<input type="email" class="form-control" id="email2" placeholder="รหัสพนักงาน"> 
-											</div>
-										</div>
-										<div class="col-md-6 col-lg-6">
-											<div class="form-group">
-												<label for="email2">ชื่อพนักงาน</label>
-												<input type="email" class="form-control" id="email2" placeholder="ชื่อพนักงาน">
-											 
-											</div>
-										</div>
-										 
-									</div>
-									<div class="row">
-										<div class="col-md-6 col-lg-6">
-											<div class="form-group">
-												<label for="email2">แผนก</label>
-												<input type="email" class="form-control" id="email2" placeholder="แผนก">
-												 
-											</div>
-										</div>
-										<div class="col-md-6 col-lg-6">
-											<div class="form-group">
-												<label for="smallSelect">ประเภทการจัดเก็บ</label>
-											  
-												<select class="form-control form-control-sm" id="smallSelect">
-													<option>1</option>
-													<option>2</option>
-													<option>3</option>
-													<option>4</option>
-													<option>5</option>
-												</select>
-										 
-												 
-											</div>
-										</div>
-									</div>
-								</div>
-
-							</div>
-						</div>
-		</div>
-          
-        </div>
-        <div class="modal-footer">
-          <button type="button" id="btncreatedata" class="btn btn-warning"  >สร้าง</button>
-          <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-  
-</div>
-  <!-- END  Modal -->
-
 			<footer class="footer">
 				<div class="container-fluid">
 					 
