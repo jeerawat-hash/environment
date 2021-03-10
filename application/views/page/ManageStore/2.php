1d0fcda5426cad2978fdc42a858324227d4bf3cb
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-10 22:33:10 +0700

    ๅๅ

--- application/views/page/ManageStore/2.php
+++ application/views/page/ManageStore/2.php
@@ -83,7 +83,7 @@
 														<div class="row">
 														<div class="col-md-6 col-lg-6">
 															<div class="form-group">
-																<label for="smallSelect">ประเภทการจัดเก็บ</label>
+																<label for="smallSelect">ป้ายทะเบียน</label>
 															
 																<select class="form-control form-control-sm" id="smallSelect">
 																	<option>1</option>
