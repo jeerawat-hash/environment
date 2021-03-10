3a5b72f4b20bf0610e64e810f2565f3a722508b9
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-10 19:41:46 +0700

    2

--- application/views/page/ManageStore/2.php
+++ application/views/page/ManageStore/2.php
@@ -86,23 +86,24 @@
 									<div class="card-title">Form Elements</div>
 								</div>
 								<div class="card-body">
-									 
-										<div class="col-md-6 col-lg-6">
+									<div class="row">
+										<div class="col-md-6 col-lg-4">
 											<div class="form-group">
 												<label for="email2">Email Address</label>
 												<input type="email" class="form-control" id="email2" placeholder="Enter Email">
 												<small id="emailHelp2" class="form-text text-muted">We'll never share your email with anyone else.</small>
 											</div>
 										</div>
-
-										<div class="col-md-6 col-lg-6d">
+									</div>
+									<div class="row">
+										<div class="col-md-6 col-lg-4">
 											<div class="form-group">
 												<label for="email2">Email Address</label>
 												<input type="email" class="form-control" id="email2" placeholder="Enter Email">
 												<small id="emailHelp2" class="form-text text-muted">We'll never share your email with anyone else.</small>
 											</div>
 										</div>
-									 
+									</div>
 								</div>
 
 							</div>
