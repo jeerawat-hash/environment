eb75ed91e6946e66fe91646a2d1635db5f16e86d
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-11 09:11:51 +0700

    add

--- application/views/page/ManageStore/0.php
+++ application/views/page/ManageStore/0.php
@@ -130,7 +130,7 @@ function initMap() {
       for (let index = 0; index < obj.length; index++) { 
         
 
-
+		/*
         const svgMarker = {
           path:
             "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
@@ -140,7 +140,7 @@ function initMap() {
           rotation: 0,
           scale: 2,
           anchor: new google.maps.Point(15, 30),
-        };
+        };*/
 
 
         var marker = new google.maps.Marker({
@@ -150,7 +150,7 @@ function initMap() {
           title: obj[index].Name, 
           LineName : obj[index].FName,
           Image : obj[index].Image,
-          icon : svgMarker,
+          icon : "https://environment.webclient.me/temp/pin-"+obj[index].Color+".svg",
           ID : obj[index].TransID/*,
           icon: {
                   url: obj[index].Image, // url
@@ -277,18 +277,8 @@ function clickroute(lati,long) {
             $(function(){
 
                 ///$('#basic-datatables').DataTable({ });
-				const svgMarker = {
-				path:
-					"M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-				fillColor: "red",
-				fillOpacity: 0.6,
-				strokeWeight: 0,
-				rotation: 0,
-				scale: 2,
-				anchor: new google.maps.Point(15, 30),
-				};
-
-				$(".SVGICON").attr("src",svgMarker);
+				  
+				$(".SVGICON").attr("src","https://environment.webclient.me/temp/pin-green.svg");
 
             });
         </script>
