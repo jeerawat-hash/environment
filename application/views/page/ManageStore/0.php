3a3531b9e5a2271b1b74a255ae92cd036fc32c7d
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-11 11:57:28 +0700

    add

--- application/views/page/ManageStore/0.php
+++ application/views/page/ManageStore/0.php
@@ -155,7 +155,7 @@ function initMap() {
 
  
     map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 14,
+      zoom: 18,
       center: { lat: 13.59934, lng: 100.59675 },
       mapTypeId: "terrain",
 	  styles: [
