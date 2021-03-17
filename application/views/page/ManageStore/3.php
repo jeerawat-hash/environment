cc3a9fe2a54f3659f866108f12207c5ae60f5912
p.jeerawat.th <s6202041520016@email.kmutnb.ac.th>
2021-03-17 15:44:52 +0700

    add

--- application/views/page/ManageStore/3.php
+++ application/views/page/ManageStore/3.php
@@ -100,7 +100,7 @@
                 for (var i = 0; i < obj.length; i++) {
  
                                          html += "<tr>"+
-                                                    "<td>คุณ "+obj[i].LineName+" <br> <img  width='10%' src='"+obj[i].LineIMG+"'></td> "+
+                                                    "<td>คุณ "+obj[i].LineName+" <br> <img  width='30%' src='"+obj[i].LineIMG+"'></td> "+
 													"<td>"+obj[i].Remark+"</td>"+
 													"<td>"+obj[i].Capacity+"</td>"+
 													"<td>"+obj[i].Comment+"</td> "+ 
